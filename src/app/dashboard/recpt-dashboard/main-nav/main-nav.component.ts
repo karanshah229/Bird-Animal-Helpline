755829f51b3c2074478588344b4a89d1427c86f4
karanshah229 <karanshah229@gmail.com>
2020-01-02 10:13:21 +0530

    Notification errors handled

--- src/app/dashboard/recpt-dashboard/main-nav/main-nav.component.ts
+++ src/app/dashboard/recpt-dashboard/main-nav/main-nav.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map, share } from 'rxjs/operators';
 import { fadeIn, fadeOut } from 'src/animations/anim_registration';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+
+interface notification {notif: string, state: string};
 
 @Component({
   selector: 'app-main-nav',
@@ -18,83 +20,73 @@ import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dial
 })
 export class MainNavComponent implements OnInit {
   username:string;
+
   //TODO: Decide Message Format
   notifs:string[] = ["#3543 accepted by ABC", '#2514 declined by Karan', '#3528 closed by Tarun'];
   notif_num:number;
-  fadeOut_notif:string[] = [];
   clear_all_notifs_btn_click:boolean = false;
-  notif_state:[{notif: string, state: string}] = [];
+  notif_state:notification[] = [];
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      share()
-    );
+  isHandset$: Subscription;
+  isMobile:boolean = false;
 
   constructor(private breakpointObserver: BreakpointObserver,
-              private dialog: MatDialog,) {
-    this.isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      share()
-    );
-    console.log(this.isHandset$);
+    private dialog: MatDialog) {
+    this.isHandset$ = this.breakpointObserver.observe([
+      Breakpoints.HandsetLandscape,
+      Breakpoints.HandsetPortrait
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.isMobile = true;
+      } else this.isMobile = false;
+    });
   }
 
   ngOnInit(){
-    //Set "normal" state to all messages
-    //for clear_all animation
-    for(let i = 0; i < this.notifs.length; i++)
-      this.fadeOut_notif[i] = "enter";
+    //Get notifs from service and Update badge
     this.notif_num = this.notifs.length;
 
     //Create Notification State
     for(let i = 0; i < this.notifs.length; i++){
-      let x = {notif: this.notifs[i], state: "enter"};
+      const x = {notif: this.notifs[i], state: "enter"};
       this.notif_state.push(x);
     }
-    console.log(this.notif_state);
-
-    this.isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      share()
-    );
   }
+
+  clear_unread_notifs(){ this.notif_num = 0; }
 
   clear_all_notifs(){
     let i = 0;
     this.clear_all_notifs_btn_click = true;
     let clear_all_notifs = setInterval(() => {
-      //this.fadeOut_notif[i] = "leave";
       this.notif_state[i] = {notif: this.notifs[i], state: "leave"};
-      console.log("asd: " + i);
       i++;
-      if(this.notifs.length === i) clearInterval(clear_all_notifs);
+      if(this.notif_state.length === i) clearInterval(clear_all_notifs);
     }, 100);
   }
 
-  clear_all_anim_end(index: number){
-    let i = index;
+  clear_all_anim_end(notif:{notif:string, state: string}){
     if(this.clear_all_notifs_btn_click){
-      console.info(i);
-      if(i === this.notifs.length - 1){
-        this.notifs = [];
-        //this.notifs = [];
-        //this.fadeOut_notif = [];
-      } else {
-        this.notif_state[i] = {notif: "", state: ""};
-        //this.notifs[i] = "";
-        //this.fadeOut_notif[i] = "";
-      }
+      if(notif === this.notif_state[this.notif_state.length - 1])
+        this.notif_state = [];
+      else notif = {notif: "", state: ""};
     }
   }
 
   openDialog(): void {
+    let dialogSize = { height: '50%', width: '40%' }
+    if(this.isMobile){
+      dialogSize = { height: '50%', width: '80%' }
+    }
     const dialogRef = this.dialog.open(All_notifications, {
-      height: '50%',
-      width: '50%',
-      data: {notifications: this.notifs}
+      height: dialogSize.height,
+      width: dialogSize.width,
+      data: this.notif_state
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+      this.notif_state = result;
     });
   }
 
@@ -106,21 +98,32 @@ export class MainNavComponent implements OnInit {
   animations: [ fadeIn(), fadeOut() ]
 })
 export class All_notifications {
-  fadeOut_notif:string[] = [];
+  notif_num:number;
+  clear_all_notifs_btn_click:boolean = false;
 
   constructor(
     public dialogRef: MatDialogRef<All_notifications>,
-    @Inject(MAT_DIALOG_DATA) public messages: string[]) {}
-
-    onNoClick(): void {
-      this.dialogRef.close();
-    }
+    @Inject(MAT_DIALOG_DATA) public notif_state: notification[]) {}
 
     ngOnInit() {
-      //Set "normal" state to all messages
-      //for clear_all animation
-      for(let i = 0; i < this.messages.length; i++)
-        this.fadeOut_notif[i] = "normal";
+    }
+
+    clear_all_notifs(){
+      let i = 0;
+      this.clear_all_notifs_btn_click = true;
+      let clear_all_notifs = setInterval(() => {
+        this.notif_state[i] = {notif: this.notif_state[i].notif, state: "leave"};
+        i++;
+        if(this.notif_state.length === i) clearInterval(clear_all_notifs);
+      }, 100);
+    }
+
+    clear_all_anim_end(notif:{notif:string, state: string}){
+      if(this.clear_all_notifs_btn_click){
+        if(notif === this.notif_state[this.notif_state.length - 1])
+          this.notif_state = [];
+        else notif = {notif: "", state: ""};
+      }
     }
 
 }
